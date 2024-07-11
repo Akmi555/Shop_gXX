9c14dff0da59e1e4a51326e49a807cb1d51ffe68
SergeyBugaenko <sergiibugaienko@gmail.com>
2024-07-11 14:37:12 +0200

    Lesson_08 controller add

---
+++ src/main/java/de/ait_tr/gxx_shop/controller/ProductController.java
@@ -0,0 +1,14 @@
+package de.ait_tr.gxx_shop.controller;
+/*
+@date 11.07.2024
+@author Sergey Bugaienko
+*/
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+}
---
+++ src/main/java/de/ait_tr/gxx_shop/domain/entity/Product.java
@@ -0,0 +1,39 @@
+package de.ait_tr.gxx_shop.domain.entity;
+/*
+@date 11.07.2024
+@author Sergey Bugaienko
+*/
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Product {
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    private boolean active;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+        return active == product.active && Objects.equals(id, product.id) && Objects.equals(title, product.title) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(title);
+        result = 31 * result + Objects.hashCode(price);
+        result = 31 * result + Boolean.hashCode(active);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product: id - %d, title - %s, price - %s, active - %s",
+                id, title, price, active ? "yes" : "no");
+    }
+}
