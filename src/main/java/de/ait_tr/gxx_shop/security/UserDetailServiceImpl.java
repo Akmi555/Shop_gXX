cefa2d2e24afb8467d9efe4a91e120591650fb36
SergeyBugaenko <sergiibugaienko@gmail.com>
2024-07-17 17:13:00 +0200

    Lesson_11 userDetailsImpl

---
+++ src/main/java/de/ait_tr/gxx_shop/security/UserDetailServiceImpl.java
@@ -0,0 +1,24 @@
+package de.ait_tr.gxx_shop.security;
+/*
+@date 17.07.2024
+@author Sergey Bugaienko
+*/
+
+import de.ait_tr.gxx_shop.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
